09256f8c67f457fff63191d5058b2ead673315f2
Felipe Toro C <ftorocastilla@gmail.com>
2023-05-19 18:35:11 -0300

    feat: style done and calculate the differences between dates

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ function App () {
   const [day, setDay] = useState('')
   const [month, setMonth] = useState('')
   const [year, setYear] = useState('')
-  const [yearResult, setYearResult] = useState('')
-  const [monthResult, setMonthResult] = useState('')
-  const [dayResult, setDayResult] = useState('')
+  const [yearResult, setYearResult] = useState('--')
+  const [monthResult, setMonthResult] = useState('--')
+  const [dayResult, setDayResult] = useState('--')
 
   const handleChangeDay = evt => {
     setDay(evt.target.value)
@@ -22,38 +22,39 @@ function App () {
   const handleSubmit = (evt) => {
     evt.preventDefault()
 
-    const date = new Date()
-    const currentYear = date.getFullYear()
-    const currentMonth = date.getMonth()
-    const currentDay = date.getDate()
+    const milisecondsPerMinute = 24 * 60 * 60 * 1000
 
-    let dayResult = 0
-    let monthResult = 0
+    const currentDate = new Date()
+    const userDate = new Date(`${year}-${month}-${day}`)
 
-    if (day < currentDay) {
-      dayResult = currentDay - day
-    }
-    if (month < currentMonth) {
-      monthResult = currentMonth - month
-    }
-    const yearResult = currentYear - year
+    let difference = currentDate - userDate
 
-    setYearResult(yearResult)
-    setMonthResult(monthResult)
-    setDayResult(dayResult)
+    const passedYear = Math.floor(difference / (milisecondsPerMinute * 365))
+
+    difference -= passedYear * milisecondsPerMinute * 365
+
+    const passedMonths = Math.floor(difference / (milisecondsPerMinute * 30))
+
+    difference -= passedMonths * milisecondsPerMinute * 30
+
+    const passedDay = Math.floor(difference / milisecondsPerMinute)
+
+    setYearResult(passedYear)
+    setMonthResult(passedMonths)
+    setDayResult(passedDay)
   }
   return (
     <>
       <article>
         <form className='Form' onSubmit={handleSubmit}>
           <label className='Form-label'> day
-            <input className='Form-input' type='number' name='day' pattern="^(0[1-9]|1[0-2])$" required onChange={handleChangeDay}></input>
+            <input className='Form-input' type='number' name='day' required placeholder='DD' onChange={handleChangeDay}></input>
           </label>
           <label className='Form-label'> month
-            <input className='Form-input' type='number' name='month' pattern="^(0[1-9]|[12][0-9]|3[01])$" required onChange={handleChangeMonth}></input>
+            <input className='Form-input' type='number' name='month' required placeholder='MM' onChange={handleChangeMonth}></input>
           </label>
           <label className='Form-label'> year
-            <input className='Form-input' type='number' name='year' pattern="^\d{4}$" required onChange={handleChangeYear}></input>
+            <input className='Form-input' type='number' name='year' required placeholder='YYYY' onChange={handleChangeYear}></input>
           </label>
         <button className='Button'> x </button>
         </form>
